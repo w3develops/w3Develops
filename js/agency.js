7cac07ad63d32333fcadbfed278daf3531911509
amansinghaljpr <amansinghaljpr@users.noreply.github.com>
2018-10-29 21:47:20 +0530

    Update agency.js

--- js/agency.js
+++ js/agency.js
@@ -1,5 +1,5 @@
 (function($) {
-  "use strict"; // Start of use strict
+  "use strict";  // Start of use strict
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
