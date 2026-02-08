b936634a3bdb31e067d9a02f4b48c6ce40c8dcf8
Jonathan <30133774+JonDevOps@users.noreply.github.com>
2026-02-08 06:38:14 -0800

    version update

---
+++ next.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;
